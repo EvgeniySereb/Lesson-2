012d4adda4b5437dec11d4773a4f30e486705ea8
Evgeniy <jenyasereb95@gmail.com>
2022-09-20 17:59:51 +0500

    2

---
+++ Task_13.cs
@@ -0,0 +1,28 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+int CreateNumber()
+{
+    int number = new Random().Next(10, 1000);
+    return number;
+}
+int DelSecondDigit(int number)
+{
+    int Ed = number % 10;
+     return Ed;
+}
+void Srav (int a)
+{
+if (a > 99)
+{
+int numberNew = DelSecondDigit(a);
+Console.WriteLine(numberNew);
+}
+else if (a < 100)
+{
+    Console.WriteLine("Третьего числа нет");
+}
+}
+
+
+int a = CreateNumber();
+Console.WriteLine(a);
+Srav(a);
