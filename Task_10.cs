90558ebfd9ced20e570fe611c7c6279b786ce816
Evgeniy <jenyasereb95@gmail.com>
2022-09-20 17:56:11 +0500

    1

---
+++ Task_10.cs
@@ -0,0 +1,20 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int CreateNumber()
+{
+    int number = new Random().Next(100, 1000);
+    return number;
+}
+
+int Del(int number)
+{
+    int Sot = number / 10;
+    int Desyatki = Sot % 10;
+
+    
+    return Desyatki ; 
+}
+
+int a = CreateNumber();
+Console.WriteLine(a);
+int numberNew = Del(a);
+Console.WriteLine(numberNew);
